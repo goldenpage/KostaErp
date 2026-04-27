f81efc6372e477b5be9a1f5be866c27210974c7e
kimjh2630 <kimjh2630@naver.com>
2026-04-27 15:43:38 +0900

    feat: VO&DAO 초기세팅

--- src/com/kostaErp/model/disposalDAO.java
+++ src/com/kostaErp/model/disposalDAO.java
@@ -1,5 +1,5 @@
 package com.kostaErp.model;
 
 public class disposalDAO {
-
+	
 }
--- src/com/kostaErp/model/disposalVO.java
+++ src/com/kostaErp/model/disposalVO.java
@@ -1,5 +1,38 @@
 package com.kostaErp.model;
 
 public class disposalVO {
-
+	private String disposalId;
+	private int disposalCountAll;
+	private int disposalPrice;
+	private String disposalDate;
+	private String reasonId;
+	private String foodMaterialId;
+	
+	public disposalVO(String disposalId, int disposalCountAll, int disposalPrice, String disposalDate, String reasonId, String foodMaterialId){
+		this.disposalId = disposalId;
+		this.disposalCountAll = disposalCountAll;
+		this.disposalPrice = disposalPrice;
+		this.disposalDate = disposalDate;
+		this.reasonId = reasonId;
+		this.foodMaterialId = foodMaterialId;
+	}
+	
+	public String getDisposalId() {
+		return disposalId;
+	}
+	public int getDisposalCountAll() {
+		return disposalCountAll;
+	}
+	public int getDisposalPrice() {
+		return disposalPrice;
+	}
+	public String getDisposalDate() {
+		return disposalDate;
+	}
+	public String getReasonId() {
+		return reasonId;
+	}
+	public String getFoodMaterialId() {
+		return foodMaterialId;
+	}
 }
