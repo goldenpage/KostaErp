482507318a1fbe2a46f907b8040349dfdf690b53
kimjh2630 <kimjh2630@naver.com>
2026-04-27 16:20:03 +0900

    feat: connection추가

--- src/com/kostaErp/model/disposalDAO.java
+++ src/com/kostaErp/model/disposalDAO.java
@@ -1,12 +1,14 @@
 package com.kostaErp.model;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class disposalDAO {
 	private Connection getConnection() throws Exception{
 		String uri = "jdbc:oracle:thin:@192.168.0.234:1521:xe";
 		String user = "kostaErp";
 		String password = "0707";
-		return 
+		return DriverManager.getConnection(uri, user, password);
 	}
 }
+
