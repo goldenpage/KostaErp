485004f1f1105b264c09f6c7b61d4fa627d48f85
kimjh2630 <kimjh2630@naver.com>
2026-04-27 16:12:40 +0900

    feat: VO&DAO 세팅

--- src/com/kostaErp/model/disposalDAO.java
+++ src/com/kostaErp/model/disposalDAO.java
@@ -1,5 +1,12 @@
 package com.kostaErp.model;
 
-public class disposalDAO {
+import java.sql.Connection;
 
+public class disposalDAO {
+	private Connection getConnection() throws Exception{
+		String uri = "jdbc:oracle:thin:@192.168.0.234:1521:xe";
+		String user = "kostaErp";
+		String password = "0707";
+		return 
+	}
 }
--- src/com/kostaErp/model/reasonVO.java
+++ src/com/kostaErp/model/reasonVO.java
@@ -1,5 +1,17 @@
 package com.kostaErp.model;
 
 public class reasonVO {
-
+	private String reasonId;
+	private String reason;
+	
+	public reasonVO(String reasonId, String reason){
+		this.reasonId = reasonId;
+		this.reason = reason;
+	}
+	public String getReasonId() {
+		return reasonId;
+	}
+	public String getReason() {
+		return reason;
+	}
 }
