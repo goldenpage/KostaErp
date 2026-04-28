0ceb1b0695167503234ced7abee55d08300c2830
차영훈 <younghun2562@naver.com>
2026-04-28 18:11:39 +0900

    feat: 초안

---
+++ src/com/kostaErp/servlet/addUserUIAction.java
@@ -0,0 +1,15 @@
+package com.kostaErp.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+public class addUserUIAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request) throws ServletException, IOException {
+		return "register.jsp";
+	}
+
+}
---
+++ src/com/kostaErp/servlet/revenueAction.java
@@ -0,0 +1,16 @@
+package com.kostaErp.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+public class revenueAction implements Action {
+
+	@Override
+	public String execute(HttpServletRequest request) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
