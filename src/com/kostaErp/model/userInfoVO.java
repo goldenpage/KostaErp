7b745f2ab49692b76607fffd43b0c830a368dcc8
KOSTA <KOSTA@DESKTOP-LBG72PF>
2026-04-29 09:32:13 +0900

    feat:login 수정

--- src/com/kostaErp/model/userInfoVO.java
+++ src/com/kostaErp/model/userInfoVO.java
@@ -1,5 +1,75 @@
 package com.kostaErp.model;
 
 public class userInfoVO {
+	private String bld;
+	private String name;
+	private int phone;
+	private String storeName;
+	private String storeType;
+	private String storeCategory;
+	private String pw;
+	private String agreementDate;
+	private String marketingDate;
 
+	public String getbld() {
+        return menuId;
+    }
+
+    public void setbld(String bld) {
+        this.bld = bld;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public void setname(String name) {
+        this.name = name;
+    }
+
+    public String getphone() {
+        return phone;
+    }
+
+    public void setstoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public int getstoreName() {
+        return storeName;
+    }
+
+    public void setstoreCategory(String storeCategory) {
+        this.storeCategory = storeCategory;
+    }
+
+    public String getstoreCategory() {
+        return storeCategory;
+    }
+
+    public void setpw(String pw) {
+        this.pw = pw;
+    }
+
+    public String agreementDate() {
+        return agreementDate;
+    }
+
+    public void setagreementDate(String agreementDate) {
+        this.agreementDate = agreementDate;
+    }
+
+    public int getagreementDate() {
+        return agreementDate;
+    }
+
+    public void setmarketingDate(String marketingDate) {
+        this.marketingDate = marketingDate;
+    }
+
+    public int getmarketingDate() {
+        return marketingDate;
+    }
+
+   
 }
