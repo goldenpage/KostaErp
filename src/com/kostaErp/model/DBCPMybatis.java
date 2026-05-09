f2f8e5f9c718d1ed2b76d04b8fbbb87adf8c5c48
ajfal <gmibook1234@gmail.com>
2026-05-09 15:30:28 +0900

    fix: 주석 추가-필요없는 주석 삭제 및 interface패키지 추가

--- src/com/kostaErp/model/DBCPMybatis.java
+++ src/com/kostaErp/model/DBCPMybatis.java
@@ -21,7 +21,6 @@ public class DBCPMybatis {
 			try {
 				in=Resources.getResourceAsStream(resource); 
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 				throw new NullPointerException("환경설정오류");
 			} 
